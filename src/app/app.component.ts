34073e3de4da6e11bbf67b723c8efb13f9b4085d
Qazi-Aimal <aimalqazi32@gmail.com>
2023-05-24 17:17:31 +0500

    adding files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Test1';
+  todos: any = [];
+  newTodo: string = '';
+
+  addText() {
+    if (this.newTodo.trim() !== '') {
+      this.todos.push(this.newTodo);
+    }
+    this.newTodo = '';
+  }
+
+  remove(i: any) {
+    this.todos.splice(i, 1);
+  }
 }
