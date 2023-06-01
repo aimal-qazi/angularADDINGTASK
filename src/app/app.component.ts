8caa14276519f008a3b05d2bc9493182ef6b601d
Qazi-Aimal <aimalqazi32@gmail.com>
2023-06-01 12:33:06 +0500

    update the files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   todos: any = [];
   newTodo: string = '';
+  counter = 0;
 
   addText() {
     if (this.newTodo.trim() !== '') {
       this.todos.push(this.newTodo);
     }
     this.newTodo = '';
+    this.counter = this.todos.length;
   }
 
   remove(i: any) {
     this.todos.splice(i, 1);
+    this.counter = this.todos.length;
   }
 }
